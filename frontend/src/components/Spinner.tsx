0cdb6b6e069790ec5adf4c2e74f3f48dd3d7c261
Phakamani Sibeko <phakamanimluleki@gmail.com>
2026-05-22 08:34:18 +0200

    Delete frontend directory

--- frontend/src/components/Spinner.tsx
+++
@@ -1,7 +0,0 @@
-export default function Spinner() {
-    return (
-        <div className="flex items-center justify-center p-12">
-            <div className="w-10 h-10 border-4 border-teal-200 border-t-teal-600 rounded-full animate-spin" />
-        </div>
-    );
-}
